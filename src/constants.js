6f4a4533366b933cc7062816b816d83abdcb589d
AnuragChandra221 <pothaluranurag2002@gmail.com>
2023-03-07 23:08:56 +0530

    backend redeployed and added:

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ export var weeks = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"]
 export var months = ["January", "February", "March", "April", "May", "June", 
                     "July", "August", "September", "October", "November", "December"
                     ];
-export var baseUrl = "https://calendar-backend-production-cb62.up.railway.app/api"
+export var baseUrl = "https://calendar-backend-production-f6ab.up.railway.app/api"
