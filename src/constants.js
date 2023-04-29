65bfbebd0afe03ad8b39ed318397f1ab73368a14
Anuragchandra221 <pothaluranurag2002@gmail.com>
2023-04-29 13:56:01 +0530

    backend hosting changed to azure

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ export var weeks = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"]
 export var months = ["January", "February", "March", "April", "May", "June", 
                     "July", "August", "September", "October", "November", "December"
                     ];
-export var baseUrl = "http://timelinecalendar.azurewebsites.net/api"
+export var baseUrl = "https://timelinecalendar.azurewebsites.net/api"
